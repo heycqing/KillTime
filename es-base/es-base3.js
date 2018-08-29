eded8716b3e4e58394b4ddf58fc972775441fc4d
cqing <SiqingIII@163.com>
2018-08-30 01:03:18 +0800

    增加css基础知识

---
+++ es-base/es-base3.js
@@ -0,0 +1,15 @@
+// 构造函数
+function Foo(name, age) {
+    this.name = name
+}
+Foo.prototype.alertName = function () {
+    alert(this.name)
+}
+// 创建示例
+var f = new Foo('zhangsan')
+f.printName = function () {
+    console.log(this.name)
+}
+// 测试
+f.printName()
+f.alertName()
