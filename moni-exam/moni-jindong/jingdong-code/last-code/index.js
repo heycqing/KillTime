fd8a35f9c05d67ecb5f2dbc3bee56ccc811ffb4e
cqing <SiqingIII@163.com>
2018-09-08 19:18:25 +0800

    fix

--- moni-exam/moni-jindong/jingdong-code/last-code/index.js
+++ moni-exam/moni-jindong/jingdong-code/last-code/index.js
@@ -22,17 +22,21 @@ function add(items){
 
         // 转换 Number 数据类型，保留2位小数  
         var fix2 =  items[i].price;
-        var num_price = new Number(fix2)  ;
-        total_price += Math.round(num_price * 100)/100
+        var num_price = new Number(fix2);
         html = '<tr><td>'+items[i].name+'</td><td>'+num_price.toFixed(2)+'</td><td><a href="javascript:void(0);">删除</a></td></tr>';
 
         tr_.innerHTML = html;
         tbody_.appendChild(tr_);
     }
-    for(var i  in tbody_.children){
-        total_price += tbody_.children[i].children[1].textContent; 
+    for(var i  in tbody_.rows){
+        
+        var temp = tbody_.rows[i].cells[1].innerText; 
+    
+        temp = Number(temp);
+        total_price +=  temp;
         console.log('1:\t'+total_price)
-
+      
+        
     }
     console.log('2:total     '+ total_price)
     tfoot_.innerHTML = '<tr><th>总计</th><td colspan="2">'+total_price+'('+tbody_.childElementCount+'件商品)</td></tr>';
