b06a681fdd6f5adf3acdc637fad25aec63792ecb
cqing <SiqingIII@163.com>
2018-11-26 15:32:30 +0800

    保存

--- mz-test/mz-m/test.js
+++ mz-test/mz-m/test.js
@@ -1,8 +1,7 @@
 var mzHeaderTouch = {
     startY: 0,
     endY: 0,
-    status: 80,
-    i:0,
+    scrollT: 0,
     mzTouchUpCss: function(){
       $('#header-wrapper').addClass('header_show')
       $('#index-nav').addClass('header_show')
@@ -15,77 +14,66 @@ var mzHeaderTouch = {
       $('#header-wrapper').removeClass('header_show')
       $('#index-nav').removeClass('header_show')
     },
-    mzScrollRemove: function(){
-      $('#header-wrapper').removeClass('header_show')
-      $('#index-nav').removeClass('header_show')
-      $('#header-wrapper').removeClass('header_hide')
-      $('#index-nav').removeClass('header_hide')
-    },
-    mzGetTouch: function(a,b){
-      var temp = a - b;
-      var result;
-      if(temp < 0 ){
-       return result = 1;
-      }else if(temp > 0){
-        return result = 2;
-      }else{
-        return result = 0;
-      }
-    },
     mzTouchStart: function(){
+      var self = this;
       document.addEventListener('touchstart',function(e){
-        this.startY = e.touches[0].clientY;
+        self.startY = e.touches[0].clientY;
       },false)   
     },
     mzTouchEnd: function(){
       var self = this;
       document.addEventListener('touchmove',function(e){
-        this.endY = e.touches[0].pageY;
-          switch(self.mzGetTouch(this.startY,this.endY)){
-            case 1:
-              self.mzTouchUpCss();
-              break;
-            case 2:
-              self.mzTouchDownCss();
-              break;
-            default:
-              return;
-              break;
+        self.endY = e.changedTouches[0].clientY;
+        var n = self.startY - self.endY;
+        if( n < 35){
+          self.mzTouchUpCss();   
+        e.preventDefault();
+               
+        }else if(n > 35){
+          self.mzTouchDownCss();    
+        e.preventDefault();
+              
+        }else{
+        e.preventDefault();
+          
+          return;
         }
+        e.preventDefault();
+        
         },false)
     },
     MzWindowMove: function(){
       var self = this;
+      var beforeScrollT = document.documentElement.scrollTop || document.body.scrollTop; 
       window.addEventListener('scroll',function(e){
-        var scrollT = document.documentElement.scrollTop || document.body.scrollTop; 
-        console.log(scrollT)
-        if( scrollT == 0){
-          self.mzScrollRemove();
-          console.log('11')
+        var afterScrollT = document.documentElement.scrollTop || document.body.scrollTop; 
+        var m =  afterScrollT - beforeScrollT;
+      if(afterScrollT < 122){
+          return;
+        }else if(afterScrollT == 0 ){
+          self.mzTouchUpCss();     
+          e.preventDefault();
         }
-        else if(scrollT - self.status < 0){
-          self.mzTouchUpCss();      
-          self.status = scrollT;
-          console.log('22')
-        }else if(scrollT - self.status > 0){
-          self.mzTouchDownCss();
-          self.status = scrollT;        
+        else if( m > 0){
+          if(afterScrollT > 0){
+            self.mzTouchDownCss();   
+            e.preventDefault();         
+          }
         }
+        else if(m < 0){
+          self.mzTouchUpCss();     
+          e.preventDefault();       
+        }
+        beforeScrollT =  afterScrollT;      
         e.preventDefault();
-        
-  
       },false)
     },
-    mzTouch: function(){ 
-        var self = this;
-        self.mzTouchStart();
-        self.mzTouchEnd();
-        // self.MzWindowMove()
-    },
     init: function(){
-      this.mzTouch();
-      
+      var self = this;
+      // self.mzTouchStart();
+      // self.mzTouchEnd();
+      self.MzWindowMove();
     }
-  }
+} 
+mzHeaderTouch.init();
   
-  mzHeaderTouch.init();
